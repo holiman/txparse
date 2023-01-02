b70fedceacb1e00671cce01bed09fa69279b3009
Martin Holst Swende <martin@swende.se>
2023-01-02 12:48:43 +0100

    generator

--- eofgenerator/main.go
+++ eofgenerator/main.go
@@ -14,7 +14,7 @@ func main() {
 func work() {
 	for {
 		var c vm.Container
-		numCodes := rand.Intn(10)
+		numCodes := rand.Intn(3)
 		for i := 0; i < numCodes; i++ {
 			code := make([]byte, rand.Intn(250))
 			rand.Read(code)
@@ -23,6 +23,10 @@ func work() {
 				Input:          uint8(rand.Intn(10)),
 				Output:         uint8(rand.Intn(5)),
 				MaxStackHeight: uint16(rand.Intn(10)),
+			}
+			if i == 0 {
+				metadata.Input = 0
+				metadata.Output = 0
 			}
 			c.Types = append(c.Types, metadata)
 		}
