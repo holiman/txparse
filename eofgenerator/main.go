acb08221a649541105eceab73bb51470928bb525
Martin Holst Swende <martin@swende.se>
2023-01-02 12:40:23 +0100

    very dumb eof generator

---
+++ eofgenerator/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/ethereum/go-ethereum/core/vm"
+)
+
+func main() {
+	work()
+}
+
+func work() {
+	for {
+		var c vm.Container
+		numCodes := rand.Intn(10)
+		for i := 0; i < numCodes; i++ {
+			code := make([]byte, rand.Intn(250))
+			rand.Read(code)
+			c.Code = append(c.Code, code)
+			var metadata = &vm.FunctionMetadata{
+				Input:          uint8(rand.Intn(10)),
+				Output:         uint8(rand.Intn(5)),
+				MaxStackHeight: uint16(rand.Intn(10)),
+			}
+			c.Types = append(c.Types, metadata)
+		}
+		fmt.Printf("%x\n", c.MarshalBinary())
+	}
+}
