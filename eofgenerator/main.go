854fe3a87f11d48862d247bf60151177d4aafb2f
Martin Holst Swende <martin@swende.se>
2023-01-02 14:56:32 +0100

    eofparse: make code generator a bit better

--- eofgenerator/main.go
+++ eofgenerator/main.go
@@ -5,6 +5,8 @@ import (
 	"math/rand"
 
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/holiman/goevmlab/ops"
+	"github.com/holiman/goevmlab/program"
 )
 
 func main() {
@@ -16,13 +18,14 @@ func work() {
 		var c vm.Container
 		numCodes := rand.Intn(3)
 		for i := 0; i < numCodes; i++ {
-			code := make([]byte, 1+rand.Intn(50))
-			rand.Read(code)
+			//code := make([]byte, 1+rand.Intn(50))
+			//rand.Read(code)
+			code, maxStack := genSimplProgam()
 			c.Code = append(c.Code, code)
 			var metadata = &vm.FunctionMetadata{
-				Input:          uint8(rand.Intn(10)),
-				Output:         uint8(rand.Intn(5)),
-				MaxStackHeight: uint16(rand.Intn(10)),
+				Input:          uint8(rand.Intn(2)),
+				Output:         uint8(rand.Intn(2)),
+				MaxStackHeight: uint16(maxStack),
 			}
 			if i == 0 {
 				metadata.Input = 0
@@ -32,4 +35,46 @@ func work() {
 		}
 		fmt.Printf("%x\n", c.MarshalBinary())
 	}
+}
+
+func genSimplProgam() ([]byte, int) {
+
+	var p = program.NewProgram()
+	var stackdepth = 0
+	valids := ops.ValidOpcodes
+	valids = append(valids, ops.OpCode(0x5c),
+		ops.OpCode(0x5d),
+		ops.OpCode(0x5e), ops.OpCode(0xb0), ops.OpCode(0xb1), ops.OpCode(0xb2))
+	maxStack := 0
+	//RJUMP    OpCode = 0x5c
+	//RJUMPI   OpCode = 0x5d
+	//RJUMPV OpCode = 0x5e
+	//CALLF OpCode = 0xb0
+	//RETF OpCode = 0xb1
+	//JUMPF OpCode= 0xb2
+	for nCases := 0; nCases < 40; nCases++ {
+		op := ops.OpCode(valids[rand.Intn(len(valids))])
+		pops := op.Pops()
+		if pops != nil {
+			if stackdepth < len(op.Pops()) {
+				for i := 0; i < len(op.Pops()); i++ {
+					p.Push(0)
+					stackdepth++
+				}
+			}
+			if stackdepth > maxStack {
+				maxStack = stackdepth
+			}
+			stackdepth -= len(op.Pops())
+			stackdepth += len(op.Pushes())
+		} else {
+			p.Push(0)
+		}
+		p.Op(op)
+		if stackdepth > maxStack {
+			maxStack = stackdepth
+		}
+	}
+	p.Op(ops.STOP)
+	return p.Bytecode(), maxStack
 }
--- go.mod
+++ go.mod
@@ -2,15 +2,18 @@ module github.com/holiman/txparse
 
 go 1.19
 
-require github.com/ethereum/go-ethereum v1.10.26
+require (
+	github.com/ethereum/go-ethereum v1.10.26
+	github.com/holiman/goevmlab v0.0.0-20221214131815-ba1151503917
+)
 
 require (
-	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/holiman/big v0.0.0-20221017200358-a027dc42d04e // indirect
-	github.com/holiman/uint256 v1.2.0 // indirect
-	golang.org/x/crypto v0.1.0 // indirect
+	github.com/holiman/uint256 v1.2.1 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 )
 
