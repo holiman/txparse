5a392983a2ec9d014145b38059dd2326ed5cb560
Martin Holst Swende <martin@swende.se>
2023-01-05 13:34:03 +0100

    eof generator

--- eofgenerator/main.go
+++ eofgenerator/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-
 	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/holiman/goevmlab/ops"
-	"github.com/holiman/goevmlab/program"
+	"github.com/holiman/goevmlab/fuzzing"
+	"math/rand"
 )
 
 func main() {
@@ -16,15 +14,21 @@ func main() {
 func work() {
 	for {
 		var c vm.Container
-		numCodes := rand.Intn(3)
+		numCodes := 1024
+		switch rand.Intn(3) {
+		case 0:
+			numCodes = 512
+		case 1:
+			numCodes = 1023
+		default:
+			numCodes = 1024
+		}
 		for i := 0; i < numCodes; i++ {
-			//code := make([]byte, 1+rand.Intn(50))
-			//rand.Read(code)
-			code, maxStack := genSimplProgam()
+			code, maxStack := fuzzing.GenerateCallFProgram()
 			c.Code = append(c.Code, code)
 			var metadata = &vm.FunctionMetadata{
-				Input:          uint8(rand.Intn(2)),
-				Output:         uint8(rand.Intn(2)),
+				Input:          uint8(0),
+				Output:         uint8(0),
 				MaxStackHeight: uint16(maxStack),
 			}
 			if i == 0 {
@@ -35,46 +39,4 @@ func work() {
 		}
 		fmt.Printf("%x\n", c.MarshalBinary())
 	}
-}
-
-func genSimplProgam() ([]byte, int) {
-
-	var p = program.NewProgram()
-	var stackdepth = 0
-	valids := ops.ValidOpcodes
-	valids = append(valids, ops.OpCode(0x5c),
-		ops.OpCode(0x5d),
-		ops.OpCode(0x5e), ops.OpCode(0xb0), ops.OpCode(0xb1), ops.OpCode(0xb2))
-	maxStack := 0
-	//RJUMP    OpCode = 0x5c
-	//RJUMPI   OpCode = 0x5d
-	//RJUMPV OpCode = 0x5e
-	//CALLF OpCode = 0xb0
-	//RETF OpCode = 0xb1
-	//JUMPF OpCode= 0xb2
-	for nCases := 0; nCases < 40; nCases++ {
-		op := ops.OpCode(valids[rand.Intn(len(valids))])
-		pops := op.Pops()
-		if pops != nil {
-			if stackdepth < len(op.Pops()) {
-				for i := 0; i < len(op.Pops()); i++ {
-					p.Push(0)
-					stackdepth++
-				}
-			}
-			if stackdepth > maxStack {
-				maxStack = stackdepth
-			}
-			stackdepth -= len(op.Pops())
-			stackdepth += len(op.Pushes())
-		} else {
-			p.Push(0)
-		}
-		p.Op(op)
-		if stackdepth > maxStack {
-			maxStack = stackdepth
-		}
-	}
-	p.Op(ops.STOP)
-	return p.Bytecode(), maxStack
 }
