0e1e76b1193ce5e7bfd2e149b77790f4a5333d96
Martin Holst Swende <martin@swende.se>
2025-05-06 10:54:07 +0200

    mainly prague updates

--- cmd/txparse/main.go
+++ cmd/txparse/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
 	"math/big"
 	"os"
 	"regexp"
-	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -15,19 +15,19 @@ import (
 
 func main() {
 	var (
-		// The input is assumed to be Cancun-enabled mainnet (chainid=1) transaction.
-		signer   = types.NewCancunSigner(new(big.Int).SetInt64(1))
+		// The input is assumed to be Prague-enabled mainnet (chainid=1) transaction.
+		signer   = types.NewPragueSigner(new(big.Int).SetInt64(1))
 		scanner  = bufio.NewScanner(os.Stdin)
 		toRemove = regexp.MustCompile(`[^0-9A-Za-z]`)
 	)
 	scanner.Buffer(make([]byte, 1024*1024), 5*1024*1024)
 	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.HasPrefix(line, "#") {
+		line := scanner.Bytes()
+		if bytes.HasPrefix(line, []byte("#")) {
 			continue
 		}
-		sanitized := toRemove.ReplaceAllString(line, "")
-		data := common.FromHex(sanitized)
+		sanitized := toRemove.ReplaceAll(line, []byte{})
+		data := common.FromHex(string(sanitized))
 		sender, err := parseSender(signer, data)
 		if err != nil {
 			fmt.Printf("err: %v\n", err)
@@ -46,9 +46,6 @@ func parseSender(signer types.Signer, data []byte) (common.Address, error) {
 	if err := extendedValidation(tx); err != nil {
 		return common.Address{}, err
 	}
-
-	//a, _ := json.MarshalIndent(tx, "", "  ")
-	//fmt.Printf("%v\n", string(a))
 
 	return signer.Sender(tx)
 }
